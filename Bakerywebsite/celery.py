dd60daf0ae525f3759d3c4d5c5a5335c1f2abd2b
banothusrikanth <banothusrikanth267@gmail.com>
2025-07-14 00:49:55 +0530

    added

--- Bakerywebsite/celery.py
+++ Bakerywebsite/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bakerywebsite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Bakerywebsite.settings")
 
-app = Celery('Bakerywebsite')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery("Bakerywebsite")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
